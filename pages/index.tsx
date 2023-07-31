9ded7883214a6a4a349d0b866da626f849798523
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-07-31 20:41:59 +0200

    refactor

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import Episodes from '@/components/episodes'
+import Heading from '@/components/heading'
 import Hero from '@/components/hero'
 import Layout from '@/components/layout'
 import Head from 'next/head'
 
-export default function Home() {
+export default function HomePage() {
   return (
     <>
       <Head>
@@ -11,10 +12,9 @@ export default function Home() {
       </Head>
       <Layout>
         <Hero/>
-        <div className="bg-white py-5 -mx-5 flex gap-3 items-center">
-          <span className="ms-5 bg-blue-600 w-6 h-6 inline-block"></span>
-          <span className="font-bold text-black text-6xl">LATEST EPISODES</span>
-        </div>
+        <Heading
+          text="latest episodes"
+        />
         <Episodes/>
       </Layout>
     </>

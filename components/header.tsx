861c97f753079799c6c60c1c8a7a618e6d5f654c
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-07-31 13:51:55 +0000

    added links, active link, refactor

--- components/header.tsx
+++ components/header.tsx
@@ -1,4 +1,14 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+const links = [
+    {name: 'Home', path: '/'},
+    {name: 'Episodes', path: '/episodes'},
+    {name: 'Contact', path: '/contact'},
+]
+
 export default function Header() {
+    const router = useRouter();
     return(
         <header className="border-b border-white">
             <div className="max-w-5xl mx-auto flex justify-between border-x border-white py-5 px-5">
@@ -8,15 +18,18 @@ export default function Header() {
                 <div>
                     <nav>
                         <ul className="flex gap-3">
-                            <li>
-                                Home
-                            </li>
-                            <li>
-                                Episodes
-                            </li>
-                            <li>
-                                Contact
-                            </li>
+                            {links.map((link, index) => (
+                                <li
+                                    key={index}
+                                >
+                                    <Link
+                                        className={`${router.asPath === link.path ? 'text-white' : 'text-gray-400'} text-gray-400 hover:text-white font-bold`}
+                                        href={link.path}    
+                                    >
+                                        {link.name}
+                                    </Link>
+                                </li>
+                            ))}
                         </ul>
                     </nav>
                 </div>

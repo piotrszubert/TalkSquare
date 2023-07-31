b8a290c661be01006a26a4b7d808a5d9b7bc7785
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-07-31 11:58:38 +0000

    created layout component, refactor

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,11 @@
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Layout from '@/components/layout'
 
 export default function Home() {
   return (
-    <main
-      className={`p-24 ${inter.className}`}
-    >
-      main
-    </main>
+    <>
+      <Layout>
+        main
+      </Layout>
+    </>
   )
 }

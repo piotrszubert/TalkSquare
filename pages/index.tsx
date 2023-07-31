61368dc356b4499b12e96aafe9b6d1f7ff642e92
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-07-31 11:59:37 +0000

    added Head component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,12 @@
 import Layout from '@/components/layout'
+import Head from 'next/head'
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>TalkWave - Home</title>
+      </Head>
       <Layout>
         main
       </Layout>

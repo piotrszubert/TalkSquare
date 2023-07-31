21dccb54e3009b63f115932604ab5ddf67f9b4ef
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-07-31 22:51:00 +0200

    created content

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,3 +23,14 @@ export default function HomePage() {
     </>
   )
 }
+
+
+// [Subscription Section]
+
+// "Subscribe now to never miss an episode of TalkSquare! Stay updated with the latest conversations, insightful guests, and captivating narratives."
+// [Subscribe Buttons]: Apple Podcasts | Spotify | Google Podcasts | Stitcher
+
+// [Contact Section]
+
+// "We'd love to hear from you! Have a suggestion, a question, or want to be a guest on our show? Connect with us."
+// [Contact Button]: Contact Us

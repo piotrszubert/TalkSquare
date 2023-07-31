8b67d18596588dded7e9f513e2f1577f54d70090
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-07-31 13:52:47 +0000

    updated text

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
         <Hero/>
         <div className="bg-white py-5 -mx-5 flex gap-3 items-center">
           <span className="ms-5 bg-blue-600 w-6 h-6 inline-block"></span>
-          <span className="font-bold text-black text-6xl">EPISODES</span>
+          <span className="font-bold text-black text-6xl">LATEST EPISODES</span>
         </div>
         <Episodes/>
       </Layout>

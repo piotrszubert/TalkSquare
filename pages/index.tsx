40f7b408c7b40f7ff634d67ac45962ffa417cdc7
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-07-31 12:59:23 +0000

    added episodes compoenent

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Episodes from '@/components/episodes'
 import Hero from '@/components/hero'
 import Layout from '@/components/layout'
 import Head from 'next/head'
@@ -14,6 +15,7 @@ export default function Home() {
           <span className="ms-5 bg-blue-600 w-6 h-6 inline-block"></span>
           <span className="font-bold text-black text-6xl">EPISODES</span>
         </div>
+        <Episodes/>
       </Layout>
     </>
   )
